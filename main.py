94541739436c1f32bf84067605a136a0d33afb72
shadowwrblx <104780811+shadowwrblx@users.noreply.github.com>
2023-05-14 19:28:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ print("""\033[95m
                                     credits: shadow.#9999
                             open source cause i dont release malware
 """)
-with open("settings.json","r") as raw:
+with open("./settings.json","r") as raw:
     settings = json.load(raw)
 
 launcher = f"{os.getenv('LOCALAPPDATA')}\\Roblox\\versions\\{settings['version']}\\RobloxPlayerLauncher.exe"
